6f892a513312b1ba46084c23f3a1c82263807633
bacem obey <bacemobey1@gmail.com>
2022-11-07 17:34:08 +0100

    add changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,18 @@ import { Component } from "@angular/core";
     templateUrl: './app.component.html',
     styleUrls: [ 'app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+    firstMediaItem = {
+        id:1,
+        name: 'Firebug',
+        meduim: 'series',
+        category: 'Science Fiction',
+        year: 2010,
+        watchedOn: 129416656384,
+        isFavorite: false
+    }
+
+    onMediaItemDelete(mediaItem) {
+
+    }
+}
--- src/app/media-item/media-item.component.ts
+++ src/app/media-item/media-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'mv-media-item',
@@ -6,9 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./media-item.component.css']
 })
 export class MediaItemComponent implements OnInit {
+  @Input() mediaItem;
+  @Output() delete = new EventEmitter();
 
   constructor() { }
-
+  
+  onDelete() {
+    console.log('deleted');
+    this.delete.emit(this.mediaItem);
+  }
   ngOnInit() {
   }
 
