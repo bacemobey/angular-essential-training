dcff881d0f2f2fdc49d4ca5356881cb9e3c92f07
bacem obey <bacemobey1@gmail.com>
2022-11-08 17:42:47 +0100

    add httpclientmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MediaItemComponent } from './media-item/media-item.component';
 import { MediaItemListComponent } from './media-item-list/media-item-list.component';
@@ -12,7 +13,8 @@ import { lookupLists, lookupListToken } from './providers';
 @NgModule({
     imports: [
         BrowserModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        HttpClientModule
         
     ],
     declarations: [
